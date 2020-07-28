97fdd4ad7c6ef1fc9e784534cf323202119fe56a
植瑞 <www.zhirui@foxmail.com>
2020-07-28 15:33:43 +0800

    fix

--- hello_web/src/main.rs
+++ hello_web/src/main.rs
@@ -1,9 +1,10 @@
-use std::io::prelude::*;
 use std::net::*;
+use std::io::prelude::*;
+use std::fs::File;
 
 #[warn(unused_variables)]
 fn main() {
-    let listener = TcpListener::bind("localhost:7788").expect("TCP 监听绑定失败！");
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("TCP 监听绑定失败！");
 
     for stream in listener.incoming() {
         let stream = stream.expect("监听流读取失败！");
@@ -14,8 +15,19 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
-
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+    let mut file = File::open(filename).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let response = format!("{}{}", status_line, contents);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
