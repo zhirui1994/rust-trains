e83a630e00640fc61af4b73ed4e36fe8b9b5d3e5
Rui Zhi <rui.zhi@compass.com>
2022-07-23 22:19:24 +0800

    update

--- thumbor/src/main.rs
+++ thumbor/src/main.rs
@@ -1,9 +1,19 @@
 mod pb;
 
+use std::sync::Arc;
+
+use bytes::Bytes;
 use pb::*;
 use axum::{extract::Path, http::StatusCode, routing::get, Router};
 use percent_encoding::percent_decode_str;
 use serde::Deserialize;
+use lru::LruCache;
+use tokio::sync::Mutex;
+use tower::{ServiceBuilder};
+use tower_http::{
+    add_extension::AddExtensionLayer
+};
+use tracing::info;
 
 #[derive(Deserialize)]
 struct Params {
@@ -11,16 +21,27 @@ struct Params {
     url: String,
 }
 
+type Cache = Arc<Mutex<LruCache<u64, Bytes>>>;
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
+    
+    let cache: Cache = Arc::new(Mutex::new(LruCache::new(1024)));
 
     let app = Router::new()
-        .route("/image/:spec/:url", get(generate));
+        .route("/image/:spec/:url", get(generate))
+        .layer(
+            ServiceBuilder::new()
+            .layer(AddExtensionLayer::new(cache))
+            .into_inner()
+        );
     
     let addr = "127.0.0.1:3000".parse().unwrap();
 
     tracing::debug!("listening on {}", addr);
+
+    info!("Listening on {}", addr);
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
